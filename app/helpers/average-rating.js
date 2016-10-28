2842ea488133a603d29e9e8d2f8e1be8c3f69705
Chris Martinez <cardamomclouds@yahoo.com>
2016-10-28 14:16:00 -0700

    start average rating helper

---
+++ app/helpers/average-rating.js
@@ -0,0 +1,8 @@
+export default Ember.Helper.helper(function(params) {
+  let value = params[0],
+      tag = '<i value="orange" id="strange" class="fa fa-star star" aria-hidden="true" data-ember-action="533"}></i>';
+      var output = tag;
+      var totalStars;
+      return  params;
+
+  return Ember.String.htmlSafe(output)});
---
+++ tests/unit/helpers/average-rating-test.js
@@ -0,0 +1,12 @@
+
+import { averageRating } from 'question-board/helpers/average-rating';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | average rating');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = averageRating([42]);
+  assert.ok(result);
+});
+
