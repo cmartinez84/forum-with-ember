ae9e1841f888f261fc94e59dafb42651c6c5cca2
Chris Martinez <cardamomclouds@yahoo.com>
2016-10-28 16:01:45 -0700

    take away input from rating

--- app/components/add-rating.js
+++ app/components/add-rating.js
@@ -2,11 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    addRating(question){
-      var rating = this.get('rating');
-      this.sendAction('addRating', question, rating)
-    },
-    click(question, rating){
+
+    addRating(question, rating){
       this.sendAction('addRating', question, rating)
     }
   }
