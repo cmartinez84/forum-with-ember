e229562ff32d005520f7e9414a74b3272ee6e83e
Chris Martinez <cardamomclouds@yahoo.com>
2016-10-30 18:49:12 -0700

    add fonts and styling to application

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-actions: {
+    editComment: false,
+    actions: {
     destroyComment(comment){
         if(confirm("are you sure you want to delete this comment?")){
             this.sendAction("destroyComment", comment);
@@ -9,9 +10,13 @@ actions: {
     },
     addRating(toRate, rating){
       this.sendAction("addRating", toRate, rating);
-  },
-  update(comment, params){
-      this.sendAction("update", comment, params)
-  }
+    },
+    update(comment, params){
+      this.sendAction("update", comment, params);
+      this.set("editComment", false);
+    },
+    showCommentEdit(){
+      this.set("editComment", true);
+    }
 }
 });
