c6cf04018bfe63daf11abcdac1e83cb60b0aabff
Chris Martinez <cardamomclouds@yahoo.com>
2016-10-30 17:56:10 -0700

    move question edit form to bottom of question, above comments, only shown on pencil icon cliclk

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        update(question){
+            var params = {
+              title: this.get('editTitle'),
+              author: this.get('editAuthor'),
+              content: this.get('editContent'),
+              date: this.get('editDate'),
+              avatar: this.get('editAvatar')
+            };
+            console.log(params);
+            this.sendAction("update", question, params);
+        }
+    }
 });
--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    questionEdit: false,
     actions: {
         addRating(toRate, rating){
             this.sendAction("addRating", toRate, rating);
@@ -9,6 +10,13 @@ export default Ember.Component.extend({
             if(confirm("Are you sure you want to do this? All comments will be deleted for this question as well")){
                 this.sendAction("destroyQuestion", question);
             }
+        },
+        update(question, params){
+            this.sendAction("update", question, params);
+            this.set("questionEdit", false);
+        },
+        showQuestionEdit(){
+            this.set("questionEdit", true);
         }
     }
 });
