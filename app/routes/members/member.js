13d926433a91bdd5e185635d67e3adba1c17e9c4
Chris Martinez <cardamomclouds@yahoo.com>
2016-11-06 13:20:27 -0800

    see member profiles on clicking from members page

--- app/routes/members/member.js
+++ app/routes/members/member.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params){
+        return Ember.RSVP.hash({
+            member: this.store.findRecord('member', params.member_id),
+        })
+    }
 });
