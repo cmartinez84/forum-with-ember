2f237433c8f4cfa8aae62184cdf900685fed7f7a
Chris Martinez <cardamomclouds@yahoo.com>
2016-11-07 00:01:42 -0800

    cut out date sort

--- app/components/all-questions.js
+++ app/components/all-questions.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    sortBy: ['date:desc'],
+    sortBy: ['title:asc'],
     sortedQuestions: Ember.computed.sort('questions', "sortBy"),
 
     actions: {
